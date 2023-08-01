8431880ad6bc8bff18cb465d22cd3ba47844bd90
Yura <you@example.com>
2023-08-01 14:46:32 +0300

    Добавьте файлы проекта.

---
+++ Models/ChatUser.cs
@@ -0,0 +1,10 @@
+﻿namespace WebChatApp.Models
+{
+    public class ChatUser
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ChatId { get; set; }
+        public Chat Chat { get; set; }
+    }
+}
