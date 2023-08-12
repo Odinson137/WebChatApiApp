6600da79f81206515c1ae27b36bf3379d62427f4
Yura <you@example.com>
2023-08-12 20:36:56 +0300

    add dto folder

---
+++ DTO/ChatDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebChatApp.DTO
+{
+    public class ChatDTO
+    {
+        public int ChatID { get; set; }
+        public string Title { get; set; }
+        public ICollection<UserDTO> Users { get; set; }
+    }
+}
---
+++ DTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace WebChatApp.DTO
+{
+    public class UserDTO
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class UserCreate
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
